19e060c70311713036ff01cd1f094370f5103573
jyl12 <joker_l_87@yahoo.com>
2022-01-18 16:37:20 +0000

    20220118:first commit

---
+++ dataCollection.py
@@ -0,0 +1,120 @@
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+ 
+TRIG = 7
+ECHO = 11
+ 
+# print ("Distance Measurement In Progress")
+ 
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+ 
+GPIO.output(TRIG, False)
+# print ("Waiting For Sensor To Settle")
+history=[]
+
+total=0
+count=0
+distance=0
+avg=0
+dist=[]
+distlist=[]
+scan=False
+
+def measureAverage():
+    history=[]
+
+    starttime=time.time()
+    for i in range(10):
+        GPIO.output(TRIG, False)
+        time.sleep(0.1)
+        #print("Waiting for sensor to settle...")
+        #print("Please wait...")
+
+        # ==========
+        # GPIO Setup
+        # ==========
+        GPIO.output(TRIG, True)
+        time.sleep(0.0001)
+        GPIO.output(TRIG, False)
+
+        #print("Sending...")
+        pulse_start=0
+        pulse_end=0
+        start=time.time()
+        pulse_start=start
+        while GPIO.input(ECHO)==0 and pulse_start-start<=0.0005:
+            pulse_start=time.time()
+        if round(pulse_start-start,3)>=0.300:
+            print(timeout)
+        else:
+            #print("Receiving...")
+            pulse_duration=0
+            while GPIO.input(ECHO)==1 and pulse_duration<2:
+                pulse_end=time.time()
+                pulse_duration=pulse_end-pulse_start
+                #print("Measuring...")
+
+        pulse_duration=pulse_end-pulse_start
+
+        # ====================
+        # Calculating Distance
+        # ====================
+        distance=pulse_duration*17150
+        distance=round(distance,2)
+        if distance < 0:
+            history.append(history[-1])
+        else:
+            history.append(distance)
+    validcount=0
+    total=0
+#     print(history)
+    for i in history:
+        if i<150:
+            validcount+=1
+            total+=i
+    if validcount>1:
+        avg_dist=round(total/validcount,2)
+#         print("Average distance:",avg_dist, "cm")
+        return avg_dist
+#         if avg_dist<=50:
+#             return([avg_dist,True,avg_dist,avg_dist])
+#         else:
+#             return([avg_dist,False,avg_dist,avg_dist])
+#     else:
+#         print("Timeout")
+#         return([999,False,999,999]) 
+ 
+def measure():
+    time.sleep(0.5)
+    GPIO.output(TRIG, True)
+    time.sleep(0.1)
+    GPIO.output(TRIG, False)
+
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
+
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+
+    distance = round(distance, 2)
+
+#     print ("Distance:",distance,"cm")
+    return distance
+ 
+if __name__ == "__main__":
+    loop = 1
+    while loop == 1:
+        x=measure() 
+        y=measureAverage()
+        print("measure:",x,", average:",y)
+ 
+# GPIO.cleanup()
+ 
---
+++ userInterface.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+from tkinter import *
+import tkinter.messagebox as tkMessageBox
+import dataCollection
+# import mysql.connector
+# from mysql.connector import Error
+
+root = Tk()
+root.title("Basic Thickness Measurement")
+ 
+width = 640
+height = 470
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+
+#=======================================VARIABLES=====================================
+THICKNESS = StringVar()
+desiredThickness = 0
+MEASURED = 0
+TARE = 0
+job = None
+#=======================================METHODS=======================================
+# def Database():
+#     global conn, cursor
+#     conn = mysql.connector.connect(host='localhost',
+#                                          database='registerdb',
+#                                          user='root',
+#                                          password='')
+#     cursor = conn.cursor()
+
+
+def Exit():
+    result = tkMessageBox.askquestion('System', 'Are you sure you want to exit?', icon="warning")
+    if result == 'yes':
+        root.destroy()
+        exit()
+def clear_text():
+    global job
+    thickness.delete(0,END)
+    lbl_measured.config(text='')
+    lbl_result.config(text='')
+    if job is not None:
+        root.after_cancel(job)
+    
+def setThickness():
+    global desiredThickness, TARE
+    TARE = 0
+    desiredThickness = float(THICKNESS.get())
+    measureThickness()
+    
+def measureThickness():
+    global MEASURED, TARE, job
+    MEASURED = dataCollection.measureAverage()
+    measureTare = round(abs(MEASURED - TARE),2)
+    lbl_measured.config(text=measureTare)
+    job = root.after(200,measureThickness)
+    if round(abs(desiredThickness - measureTare),2) > 0.5:
+        lbl_result.config(text='Warning.')
+    else:
+        lbl_result.config(text='Passed.')
+
+    
+def tareThickness():
+    global MEASURED, TARE
+    TARE = MEASURED
+
+
+#=====================================FRAMES====================================
+TitleFrame = Frame(root, height=100, width=640, bd=1, relief=SOLID)
+TitleFrame.pack(side=TOP)
+RegisterFrame = Frame(root)
+RegisterFrame.pack(side=TOP, pady=20)
+
+#=====================================LABEL WIDGETS=============================
+lbl_title = Label(TitleFrame, text="Thickness Measurement", font=('arial', 18), bd=1, width=640)
+lbl_title.pack()
+lbl_desired = Label(RegisterFrame, text="Desired thickness (cm):", font=('arial', 18), bd=18)
+lbl_desired.grid(row=1)
+# lbl_password = Label(RegisterFrame, text="Password:", font=('arial', 18), bd=18)
+# lbl_password.grid(row=2)
+lbl_thickness = Label(RegisterFrame, text="Measured thickness (cm):", font=('arial', 18), bd=18)
+lbl_thickness.grid(row=3)
+lbl_measured = Label(RegisterFrame, text="0.00", font=('arial', 18), bd=18)
+lbl_measured.grid(row=3, column=1)
+lbl_result = Label(RegisterFrame, text="", font=('arial', 25))
+lbl_result.grid(row=4, columnspan=2)
+
+
+#=======================================ENTRY WIDGETS===========================
+thickness = Entry(RegisterFrame, font=('arial', 20), textvariable=THICKNESS, width=15)
+thickness.grid(row=1, column=1)
+# pass1 = Entry(RegisterFrame, font=('arial', 20), textvariable=PASS, width=15, show="*")
+# pass1.grid(row=2, column=1)
+# name = Entry(RegisterFrame, font=('arial', 20), textvariable=NAME, width=15)
+# name.grid(row=3, column=1)
+
+#========================================BUTTON WIDGETS=========================
+btn_set=Button(RegisterFrame, font=('arial', 20), text="Set", command=setThickness)
+btn_set.grid(row=2, columnspan=2)
+btn_tare=Button(RegisterFrame, font=('arial', 20), text="Tare", command=tareThickness)
+btn_tare.grid(row=6, columnspan=2)
+btn_clear=Button(RegisterFrame, font=('arial', 20), text="Clear", command=clear_text)
+btn_clear.grid(row=8, columnspan=2, padx=30,pady=30)
+#========================================MENUBAR WIDGETS==================================
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Exit", command=Exit)
+menubar.add_cascade(label="File", menu=filemenu)
+root.config(menu=menubar)
+
+
+#========================================INITIALIZATION===================================
+if __name__ == '__main__':
+    root.mainloop()
+   
